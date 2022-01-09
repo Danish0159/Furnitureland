556dab9314916bbe6d2581fb1b5377b2e4198adb
Danish <danish01132@gmail.com>
2022-01-09 06:39:22 +0500

    Initial Commit Updated

--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './styles/main.css'
-import './styles/bootstrap.min.css'
-
-
-// JavaScrip and JQuery File is required.
-// import './scripts/bootstrap.bundle.min.js'
-// import './scripts/jquery-3.3.1.min.js'
-
-
+import './index.css'
 import App from './App'
 import { ProductsProvider } from './context/products_context'
 import { FilterProvider } from './context/filter_context'
 import { CartProvider } from './context/cart_context'
 import { UserProvider } from './context/user_context'
 import { Auth0Provider } from '@auth0/auth0-react'
-
 
 ReactDOM.render(
     <ProductsProvider>
